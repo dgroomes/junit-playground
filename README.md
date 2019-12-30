d68724bd39fbc497552593dbe9afce4c44ee6b1a
David Groomes <dwgroomes@gmail.com>
2019-12-29 21:50:08 -0600

    Automate the generation of the test class path (#4)

--- README.md
+++ README.md
@@ -4,13 +4,17 @@ Learning and exploring JUnit <https://junit.org/junit5/>.
 
 ## Instructions
 
-Run the tests with `./gradlew test`
+Run the tests using Gradle with `./gradlew test`
+
+Run the tests using the standalone JUnit Console Launcher with `./test.sh`. Normally, you would want to use Gradle 
+itself to launch and run the test suite, but in some cases you might not have Gradle or cannot use Gradle. For example,
+you might want to use a newer version of Java that Gradle does not yet support. Gradle generally does not get support 
+for newer versions of Java until some wait time after it launches. So if you can't wait and you want to run JUnit tests,
+this illustrates a working example of the standalone tool.
 
 ## WishList
 
 General clean ups, TODOs and things I wish to implement for this project:
 
-* Automate classpath config in the JUnit standalone console by using Gradle (some task like printTestRuntimeClasspath 
-  (that's made up)). Tip: write the classpath to a file and leverage https://junit.org/junit5/docs/current/user-guide/#running-tests-console-launcher-argument-files
 * Optimize the CI build (Github Actions). The set up Java step is slow (20s), why even do it? Maybe make a base Docker 
   image tailor-made for Gradle/Java projects. 
--- test.sh
+++ test.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 # Run the tests using the standalone JUnit console
 
-# Build the soure code (including tests), install dependencies, and print path to the standalone JUnit Console Launcher
-./gradlew compileTestJava installDist printJunitLauncherPath
+# Build the soure code (including tests), install dependencies, print path to the standalone JUnit Console Launcher,
+# and print the test class path to a file.
+./gradlew compileTestJava installDist printJunitLauncherPath printTestClassPath
 
-LAUNCHER_PATH_FILE=build/junit-launcher.path
-if [[ -f "$LAUNCHER_PATH_FILE" ]]; then
-  LAUNCHER_PATH=$(cat "$LAUNCHER_PATH_FILE")
-else
-  echo >&2 "Expected to find file '$LAUNCHER_PATH_FILE' but did not"
-  exit 1
-fi
+LAUNCHER_PATH_FILE=build/junit-launcher-path.txt
+TEST_CLASSPATH_FILE=build/test-classpath.txt
+
+assertFileExists() {
+  local file=$1
+  if [[ ! -f "$file" ]]; then
+    echo >&2 "Expected to find file '$file' but did not"
+    exit 1
+  fi
+}
+
+assertFileExists $LAUNCHER_PATH_FILE
+assertFileExists $TEST_CLASSPATH_FILE
+
+LAUNCHER_PATH=$(cat "$LAUNCHER_PATH_FILE")
 
 java -jar "$LAUNCHER_PATH" \
   --include-engine junit-jupiter \
   --fail-if-no-tests \
   --scan-classpath \
-  --classpath build/classes/java/main \
-  --classpath build/classes/java/test \
-  --classpath build/install/junit-playground/lib/slf4j-api* \
-  --classpath build/install/junit-playground/lib/slf4j-simple*
+  --classpath @$TEST_CLASSPATH_FILE
